79b5e29f0a00f2ec7948d1cf5b96da7232c54edc
hyungilee <mike.hyungi.lee@gmail.com>
2020-05-10 20:25:41 +0900

    [update] 打刻チェック画面のレイアウトをアップデートしました。

--- app/src/main/java/com/hyungilee/commutingmanagement/ui/commutingtimeregistration/CommutingTimeRegistrationFragment.kt
+++ app/src/main/java/com/hyungilee/commutingmanagement/ui/commutingtimeregistration/CommutingTimeRegistrationFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.database.FirebaseDatabase
@@ -15,7 +18,7 @@ import com.hyungilee.commutingmanagement.data.entity.CommutingData
 import com.hyungilee.commutingmanagement.data.repository.CommutingDatabaseRepository
 import kotlinx.android.synthetic.main.commuting_time_registration_fragment.*
 
-class CommutingTimeRegistrationFragment : Fragment() {
+class CommutingTimeRegistrationFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     companion object {
         fun newInstance() = CommutingTimeRegistrationFragment()
@@ -23,11 +26,27 @@ class CommutingTimeRegistrationFragment : Fragment() {
 
     private lateinit var viewModel: CommutingTimeRegistrationViewModel
 
+    private lateinit var spinner: Spinner
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.commuting_time_registration_fragment, container, false)
+        val view = inflater.inflate(R.layout.commuting_time_registration_fragment, container, false)
+        // Spinnerを初期化
+        spinner = view.findViewById(R.id.option1_spinner)
+        // Spinnerのアイテムクリックリスナー
+        spinner.onItemSelectedListener = this
+        // Spinnerに表示するリストをString arrayから取得して初期化する
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.commuting_option_array,
+            android.R.layout.simple_spinner_item
+        ).also {adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -37,15 +56,27 @@ class CommutingTimeRegistrationFragment : Fragment() {
         val viewModelFactory = CommutingTimeRegistrationViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(CommutingTimeRegistrationViewModel::class.java)
 
-        val ref = FirebaseDatabase.getInstance().getReference("commuting_database")
-        // unique id
-        val userId = ref.push().key
-        // new commuting data
-        val commutingData = CommutingData(userId!!, "S", "5/5", "出勤", "07:00", "00:00")
+//        val ref = FirebaseDatabase.getInstance().getReference("commuting_database")
+//        // unique id
+//        val userId = ref.push().key
+//        // new commuting data
+//        val commutingData = CommutingData(userId!!, "S", "5/5", "出勤", "07:00", "00:00")
+//
+//        ref.child(userId).setValue(commutingData).addOnCompleteListener {
+//            Toast.makeText(context, "Commuting data saved successfully", Toast.LENGTH_LONG).show()
+//        }
 
-        ref.child(userId).setValue(commutingData).addOnCompleteListener {
-            Toast.makeText(context, "Commuting data saved successfully", Toast.LENGTH_LONG).show()
-        }
+
+    }
+
+    // Spinnerのアイテムクリックイベントメソット
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 
 }
---
+++ app/src/main/java/com/hyungilee/commutingmanagement/ui/login/LoginActivity.kt
@@ -0,0 +1,226 @@
+package com.hyungilee.commutingmanagement.ui.login
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hyungilee.commutingmanagement.R
+import com.hyungilee.commutingmanagement.databinding.ActivityLoginBinding
+import com.hyungilee.commutingmanagement.utils.BaseActivity
+
+
+class LoginActivity : BaseActivity(), View.OnClickListener {
+
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+    // login_id and password binding
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setProgressBar(binding.progressBar)
+
+        // buttons binding
+        binding.emailSignInButton.setOnClickListener(this)
+        binding.emailCreateAccountButton.setOnClickListener(this)
+        binding.signOutButton.setOnClickListener(this)
+        binding.verifyEmailButton.setOnClickListener(this)
+        binding.reloadButton.setOnClickListener(this)
+
+        // Firebase Auth 初期化
+        auth = Firebase.auth
+
+    }
+
+    // user チェック
+    override fun onStart() {
+        super.onStart()
+        // userがログインしているかどうかを確認する
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?){
+        hideProgressBar()
+        if(user != null){
+            binding.status.text = "Email User: $user.email (verified: ${user.isEmailVerified}) "
+            binding.detail.text = "Firebase UID: ${user.uid}"
+            binding.emailPasswordButtons.visibility = View.GONE
+            binding.emailPasswordFields.visibility = View.GONE
+            binding.signedInButtons.visibility = View.VISIBLE
+
+            if(user.isEmailVerified){
+                binding.verifyEmailButton.visibility = View.GONE
+            }else{
+                binding.verifyEmailButton.visibility = View.VISIBLE
+            }
+        }else{
+            binding.status.text = "Signed out"
+            binding.detail.text = null
+
+            binding.emailPasswordButtons.visibility = View.VISIBLE
+            binding.emailPasswordFields.visibility = View.VISIBLE
+            binding.signedInButtons.visibility = View.GONE
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.emailCreateAccountButton -> {
+                createAccount(binding.fieldEmail.text.toString(), binding.fieldPassword.text.toString())
+            }
+            R.id.emailSignInButton -> signIn(binding.fieldEmail.text.toString(), binding.fieldPassword.text.toString())
+            R.id.signOutButton -> signOut()
+            R.id.verifyEmailButton -> sendEmailVerification()
+            R.id.reloadButton -> reload()
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressBar()
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // [START_EXCLUDE]
+                hideProgressBar()
+                // [END_EXCLUDE]
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.fieldEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.fieldEmail.error = "Required."
+            valid = false
+        } else {
+            binding.fieldEmail.error = null
+        }
+
+        val password = binding.fieldPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.fieldPassword.error = "Required."
+            valid = false
+        } else {
+            binding.fieldPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressBar()
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // [START_EXCLUDE]
+                if (!task.isSuccessful) {
+                    binding.status.text = "Authentication failed."
+                }
+                hideProgressBar()
+                // [END_EXCLUDE]
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun sendEmailVerification() {
+        // Disable button
+        binding.verifyEmailButton.isEnabled = false
+
+        // Send verification email
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // [START_EXCLUDE]
+                // Re-enable button
+                binding.verifyEmailButton.isEnabled = true
+
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                // [END_EXCLUDE]
+            }
+        // [END send_email_verification]
+    }
+
+    private fun reload() {
+        auth.currentUser!!.reload().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                updateUI(auth.currentUser)
+                Toast.makeText(this@LoginActivity,
+                    "Reload successful!",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(TAG, "reload", task.exception)
+                Toast.makeText(this@LoginActivity,
+                    "Failed to reload user.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
+    }
+
+    companion object{
+        private const val TAG = "EmailPassword"
+    }
+}
